42688d5548842e6b7dc635a716ffd7b7a12927bf
Bulbuk <sbulbuk@gmail.com>
2022-09-28 22:42:17 +0300

    burger menu test on index.html

--- js/index.js
+++ js/index.js
@@ -27,11 +27,28 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+let menu = document.getElementById("navigation");
 
+let a = 1;
 
 let burgerMenu = document.getElementById("burgerMenu");
 burgerMenu.addEventListener("click", appearBurgerMenu);
 
+let burgerImg = document.getElementById("burger_img");
+
 function appearBurgerMenu(){
- console.log("appear");
+    a++;
+    if(a %2 === 0){
+      menu.style.cssText=`
+      display: grid;
+      `;
+      burgerMenu.src="img/cross.png";
+    }else{
+      menu.style.cssText=`
+      display: none;
+      `;
+      burgerMenu.src="img/Vector (1).png";
+    }
+
+    
 }
